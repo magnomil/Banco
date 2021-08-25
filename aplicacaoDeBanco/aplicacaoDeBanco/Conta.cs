17acffb9f227ffc8d5a69ac066d1799191f9b2ac
magnomil <67349729+magnomil@users.noreply.github.com>
2021-08-25 19:49:45 -0300

    Adicionar arquivos de projeto.

---
+++ aplicacaoDeBanco/aplicacaoDeBanco/Conta.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace aplicacaoDeBanco
+{
+    class Conta
+    {
+        public int NumeroDaConta { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set;}
+
+
+        public Conta(int numeroDaConta, string titulardaconta, double depositoInicial){
+
+            NumeroDaConta = numeroDaConta;
+            Titular = titulardaconta;
+            //Saldo = saldo;
+            Deposito(depositoInicial);
+        }
+
+
+        public Conta(int numeroDaConta, string titularDaConta)
+        {
+            NumeroDaConta = numeroDaConta;
+            Titular = titularDaConta;
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia + 5.0;
+        }
+        public override string ToString()
+        {
+            return
+                "Número da Conta: "
+                + NumeroDaConta
+                + ", Titular: "
+                + Titular
+                + ", Saldo: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+           
+}
+
+
---
+++ aplicacaoDeBanco/aplicacaoDeBanco/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Globalization;
+
+namespace aplicacaoDeBanco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // projeto de banco
+
+            Conta contaBancaria;
+            
+            Console.Write("Entre o número da conta:");
+
+            int numeroDaConta = int.Parse(Console.ReadLine());
+           
+
+            Console.WriteLine("Entre o titular da conta:");
+           
+            string titulardaconta = Console.ReadLine();
+
+
+            Console.WriteLine("Haverá depósito inicial? Para sim digite s ou para não digite n ");
+
+            char resp = char.Parse(Console.ReadLine()); 
+            
+            if (resp =='s' || resp =='S')
+            {
+                Console.WriteLine("Digite o quanto quer depositar");
+                double depositoInicial = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                contaBancaria = new Conta(numeroDaConta, titulardaconta, depositoInicial );
+            }
+            else
+            {
+                contaBancaria = new Conta(numeroDaConta, titulardaconta);
+            }
+
+
+            Console.WriteLine("Dados Atualizados da conta: ");
+            Console.WriteLine(contaBancaria);
+
+
+            Console.Write("Entre com o valor para Depósito");
+            double quantia= double.Parse( Console.ReadLine(),CultureInfo.InvariantCulture);
+            contaBancaria.Deposito(quantia);
+
+            Console.WriteLine("Dados Atualizados da conta: ");
+            Console.WriteLine(contaBancaria);
+
+
+            Console.Write("Entre com o valor para Saque");
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            contaBancaria.Saque(saque);
+
+            Console.WriteLine("Dados Atualizados da conta: ");
+            Console.WriteLine(contaBancaria);
+
+
+
+
+
+
+
+
+        }
+    }
+}
